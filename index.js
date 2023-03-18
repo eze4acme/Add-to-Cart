6e339ceee3966b5aaa364066e2ec5c569d77fdf1
Eze Onyebuchi <eze4acme@gmail.com>
2023-03-18 20:26:08 +0100

    update index.js
    Deleted dead code in index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// databaseURL = 'https://playground-8bfc1-default-rtdb.europe-west1.firebasedatabase.app/'
-// initializeApp = https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js
-// getDatabase = "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
@@ -25,17 +21,12 @@ onValue(shoppingListInDB, function(snapshot) {
     clearShoppingListEl()
     itemArray.map(item => {
         let currentItem = item
-        // let currentItemID = currentItem[0]
-        // let currentItemValue = currentItem[1]
         renderShoppingListEl(currentItem)
     })
-    // clearInputFieldEl()
   }else{
     shoppingListEl.innerHTML = `<h1>No items here... yet</h1>`
   }
 }) 
-
-
 function addItem() {
     let inputValue = inputField.value
     push(shoppingListInDB, inputValue)
@@ -50,7 +41,6 @@ function clearInputFieldEl() {
 }
 
 function renderShoppingListEl(item) {
-    // shoppingListEl.innerHTML += `<li>${inputValue}</li>`
     let itemID = item[0]
     let itemValue = item[1]
     let newEl = document.createElement('li')
